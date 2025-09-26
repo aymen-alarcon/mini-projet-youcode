321b19473ae354f81013ab9f546cf52f0a238e27
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-09-26 16:08:32 +0100

    `Added functionality to a C program for managing student grades, including adding students, entering grades, displaying student information, and calculating class averages.`

--- project.c
+++ project.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+int calculerMoyenneGenerale();
+int afficherBulletin();
+int afficherEtudiants();
+int calculerMoyenneEtudiant();
+int saisirNotes();
+int ajouterEtudiant();
+// files(prototypes, service menu) ; optimization; mysql
 struct student{
     int id;
     char firstname[10];
@@ -7,18 +14,15 @@ struct student{
     float grade[4];
     float average;
 };
-
-int main(){
-    struct student class[30];
-    int choice;
-    int size = sizeof(class) / sizeof(class[0]);
-    int search;
     int studentCount = 0; 
     float class_average = 0;
-    float sum = 0;
+    struct student class[30];
+    int size = sizeof(class) / sizeof(class[0]);
+
+int main(){
+    int choice;
 
     do{
-        debut:
         printf("\nHello! What would you like to do today?\n");
         printf("Press 1 to Add a student\n");
         printf("Press 2 to add students grades\n");
@@ -31,20 +35,45 @@ int main(){
 
         switch (choice){
             case 1 :
-                for (int i = 0; i < size; i++)
-                {
-                        printf("Enter student's ID: ");
-                        scanf("%d", &class[studentCount].id);
-                        printf("Enter first name: ");
-                        scanf("%s", class[studentCount].firstname);
-                        printf("Enter last name: ");
-                        scanf("%s", class[studentCount].lastname);
-                        studentCount++;
-                        goto debut;
-                }
+                ajouterEtudiant();
             break;
-            case 2 :  
-                printf("could you please specify which students grade(by entering his ID)");
+            case 2 : 
+                 saisirNotes();
+                break;
+            case 3 :
+                afficherEtudiants();
+            break;
+            case 4 :
+                afficherBulletin();
+            break;
+            case 5:
+                calculerMoyenneGenerale();
+            break;
+            case 0: 
+            return 0;
+        }
+    }while (choice != 0);
+}
+
+int ajouterEtudiant(){
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter student's ID: ");
+        scanf("%d", &class[studentCount].id);
+        printf("Enter first name: ");
+        scanf("%s", class[studentCount].firstname);
+        printf("Enter last name: ");
+        scanf("%s", class[studentCount].lastname);
+        studentCount++;
+        main();
+    }
+}
+
+int saisirNotes(){
+        float sum = 0;
+
+        int search;
+            printf("could you please specify which students grade(by entering his ID)");
                 scanf("%d", &search);
                 
                 int index = -1;
@@ -59,64 +88,74 @@ int main(){
                     printf("Student not found!\n");
                 } else {
                     for (int g = 0; g < 4; g++) {
-                        printf("Enter grade %d: ", g + 1);
-                        scanf("%f", &class[index].grade[g]);
+                        do
+                        {
+                            printf("Enter grade %d: ", g + 1);
+                            scanf("%f", &class[index].grade[g]);
+                            if (class[index].grade[g] < 1 || class[index].grade[g] > 20)
+                            {
+                                printf("could you please enter a valid number");
+                            }
+                            
+                        } while (class[index].grade[g] < 1 || class[index].grade[g] > 20);
+                        
                         sum = sum + class[index].grade[g];
                     }
                     class[index].average = sum / 4;
                     printf("%.2f\n", class[index].average);
                 }
-                goto debut;
-                break;
-                case 3 :
-                for (int i = 0; i < studentCount; i++) {
-                    printf("ID: %d | first name: %s | last name:  %s | ", class[i].id, class[i].firstname, class[i].lastname);
-                    for (int g = 0; g < 4; g++) {
-                        printf("[%.2f] ", class[i].grade[g]);
-                        sum = sum + class[index].grade[g];
-                    }                    
-                    
-                    class[index].average = sum / 4;
-                    printf("%.2f\n", class[index].average);
-                    printf("\n");
-                }   
-                goto debut;
-                break;
-            case 4 :
-                printf("could you please specify which students grade(by entering his ID)");
-                scanf("%d", &search);                
-                    int ind = -1;
-                    for (int i = 0; i < studentCount; i++) {
-                        if (class[i].id == search) {
-                            ind = i;
-                            break;
-                        }
-                    }
+                    for (int i = 0; i < 4; i++)
+    {
+        class[index].average = sum / 4;
+    }
+    printf("%.2f\n", class[index].average);
+    printf("\n");
+}
 
-                    if (ind == -1) {
-                        printf("Student not found!\n");
-                    } else {
-                        for (int g = 0; g < 4; g++) {
-                            printf("note %d : %.2f\n", g + 1, class[ind].grade[g]);
-                        }
-                        printf("%.2f\n", class[ind].average);
-                    }
-                    goto debut;
-                break;
-                case 5:
-                    if (studentCount == 0) {
-                        printf("No students to calculate average.\n");
-                    } else {
-                        float total = 0;
-                        for (int i = 0; i < studentCount; i++) {
-                            total += class[i].average;
-                        }
-                        float class_average = total / studentCount;
-                        printf("Class average: %.2f\n", class_average);
-                    }
-                
-                    goto debut;
-                break;
+int afficherBulletin(){
+    int search;
+    printf("could you please specify which students grade(by entering his ID)");
+    scanf("%d", &search);       
+    int ind = -1;
+    for (int i = 0; i < studentCount; i++) {
+        if (class[i].id == search) {
+            ind = i;
+            break;
+            }
         }
-    }while (choice != 0);
+        if (ind == -1) {
+            printf("Student not found!\n");
+        } else {
+            for (int i = 0; i < studentCount; i++)
+            {
+                for (int g = 0; g < 4; g++) {
+                    printf("note %d : %.2f\n", g + 1, class[ind].grade[g]);
+                }
+                printf("%.2f\n", class[ind].average);
+            }
+        }
+}
+
+int calculerMoyenneGenerale(){
+    if (studentCount == 0) {
+        printf("No students to calculate average.\n");
+    } else {
+        float total = 0;
+        for (int i = 0; i < studentCount; i++) {
+            total += class[i].average;
+        }
+        float class_average = total / studentCount;
+        printf("Class average: %.2f\n", class_average);
+    }
+}
+
+int afficherEtudiants(){
+    for (int i = 0; i < studentCount; i++) {
+        printf("ID: %d | first name: %s | last name:  %s | [", class[i].id, class[i].firstname, class[i].lastname);
+        for (int g = 0; g < 3; g++) {
+            printf("%.2f, ", class[i].grade[g]);
+        }           
+        printf("%.2f] |", class[i].grade[3]);
+        printf("\t%.2f\n", class[i].average);
+    } 
 }
